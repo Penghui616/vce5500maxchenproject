ff4b37bd5a174a18d3d7fd561393f7ded5b76aab
Penghui616 <chenpe3@udmercy.edu>
2025-02-05 06:05:04 +0800

    Update can_receiver.py

--- TEST CAN/can_receiver.py
+++ TEST CAN/can_receiver.py
@@ -1,10 +1,9 @@
 import can
 
-# 连接到虚拟 CAN 总线 vcan0
 bus = can.interface.Bus(channel='vcan0', bustype='socketcan')
 
 print("Listening for CAN messages...")
 while True:
-    message = bus.recv()  # 接收 CAN 消息
+    message = bus.recv() 
     print(f"Received CAN Message: ID={hex(message.arbitration_id)}, Data={message.data}")
 
