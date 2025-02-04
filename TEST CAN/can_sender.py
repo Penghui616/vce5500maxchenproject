eb291d93ebe98d744148367cd8db9ee9a75a039d
Penghui616 <chenpe3@udmercy.edu>
2025-02-05 06:05:41 +0800

    Update can_sender.py

--- TEST CAN/can_sender.py
+++ TEST CAN/can_sender.py
@@ -1,16 +1,15 @@
 import can
 import time
 
-# 连接到虚拟 CAN 总线 vcan0
 bus = can.interface.Bus(channel='vcan0', bustype='socketcan')
 
 while True:
     message = can.Message(
         arbitration_id=0x123,  # CAN ID
-        data=[0xDE, 0xAD, 0xBE, 0xEF],  # 数据
-        is_extended_id=False  # 标准帧
+        data=[0xDE, 0xAD, 0xBE, 0xEF],  
+        is_extended_id=False  
     )
-    bus.send(message)  # 发送消息
+    bus.send(message)  
     print("Sent CAN Message")
     time.sleep(1)
 
